808daa2a76697c7d78df42fcef8426ee98377a60
Brandy Nicholson <brandy.nicholson@sparkhound.com>
2018-09-12 21:17:29 -0500

    html skeleton

---
+++ app/data/friends.js
@@ -0,0 +1,33 @@
+<!DOCTYPE html></body>
+
+<html lang="en-us">
+
+
+
+<head>
+
+    <!-- <link rel="stylesheet"  href="assets/css/reset.css"> -->
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
+
+        crossorigin="anonymous">
+
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.1/css/all.css" integrity="sha384-O8whS3fhG2OnA5Kas0Y9l3cfpmYjapjI0E4theH4iuMD+pLhbf6JI0jIMfYcK3yZ"
+
+        crossorigin="anonymous">   
+
+    <link rel="stylesheet" href="assets/css/style.css">
+
+
+
+    <meta charset="UTF-8">
+
+    <title>Portfolio</title>
+
+</head>
+
+
+
+
+
+<body>
